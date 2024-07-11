0d22f66921abcd15646de0aa0d0d9eae5db6d098
sanghyeon <ppppp5001@naver.com>
2024-07-11 20:22:31 +0900

    Create Xcode Project

---
+++ ColorTestApp/ColorTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTestAppApp.swift
+//  ColorTestApp
+//
+//  Created by 박상현 on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
